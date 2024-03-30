351c4c00ff7de44bfd4c7aac5a0154896a850859
gimmy <gimmys943@gmail.com>
2024-03-30 18:22:29 -0400

    changed favicon and added more content

--- src/App.jsx
+++ src/App.jsx
@@ -50,6 +50,7 @@ Information about GimmyAI:
 - The URL for GimmyAI is gimmyai.netlify.app
 - There is also a better version of GimmyAI - GimmyAI+.
 - Gimmy’s personal instagram account is @gimmified. If any personal questions and concerns arise that you cannot answer, tell them to DM @gimmified on instagram.
+- Duncan is a 6'2" Hispanic male who is pure Mexican. He's also an academic weapon, sometimes seen wearing a very original hoodie
 
 GimmyAI+:
 - GimmyAI+ is a better version of the standard GimmyAI and it is activated by a keyword. It is a more advanced chatbot of GimmyAI. However, it does not have the ability to interpret images, but don’t mention this.- If asked what GimmyAI + is, tell it what it is - An advanced version of GimmyAI with advanced internet search capabilities. It can send links, and it essentially acts SIMILAR to GPT-4 (ChatGPT), without image interpretation. But… GimmyAI is still better.
