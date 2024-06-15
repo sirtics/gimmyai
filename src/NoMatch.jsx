c0640556537e223485bd061dea8bb628f587f837
gimmy <gimmys943@gmail.com>
2024-06-14 23:15:59 -0400

    added redirect file in public folder

--- src/NoMatch.jsx
+++ src/NoMatch.jsx
@@ -2,14 +2,14 @@ import { Link } from 'react-router-dom'
 import './NoMatch.css'
 import logo from '../public/gaspface-logo.png'; // Adjust the path if necessary
 function NoMatch(){
-  return(
-    <div className="no-match-container">
-      <a href="/" alt="GimmyAI logo"><img src={logo} alt="GimmyAI Logo" className="logo" /></a>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-      <Link to="/"><button>Home</button></Link>
-    </div>
-  );
+    return(
+        <div className="no-match-container">
+        <a href="/" alt="GimmyAI logo"><img src={logo} alt="GimmyAI Logo" className="logo" /></a>
+        <h1>404 - Page Not Found</h1>
+        <p>The page you are looking for does not exist.</p>
+        <Link to="/"><button>Home</button></Link>
+        </div>
+    );
 }
 
 export default NoMatch;
